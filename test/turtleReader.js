4e54c2b09d966ee4b13e522bf4d63fee57465d83
Lars Reimann <mail@lars-reimann.eu>
2016-08-18 15:58:46 +0200

    Updated tests to Mocha v3

--- test/turtleReader.js
+++ test/turtleReader.js
@@ -1,4 +1,6 @@
 import chai, {expect} from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
 
 import {Graph, Triple, BlankNode, NamedNode, Literal, xmlSchemaTypes as xsd, TurtleReader} from "../src/index.js";
 
@@ -17,12 +19,14 @@ describe("TurtleReader", function () {
     });
 
     describe("#parse", function () {
-        it("should parse valid turtle", async function (done) {
+        it("should parse valid turtle", function () {
             const t0 = new Triple(new BlankNode("b1"), new NamedNode("n1"), new Literal("l1"));
             const t1 = new Triple(new NamedNode("b1"), new NamedNode("n1"), new Literal("1", {datatype: xsd.integer}));
             const t2 = new Triple(new BlankNode("b1"), new NamedNode("n2"), new Literal("l1", {language: "en"}));
-            try {
-                const {graph, profile} = await this.reader.parse(this.s0);
+
+            const p = this.reader.parse(this.s0);
+
+            return p.then(({graph, profile}) => {
                 expect(graph.hasTriple(t0)).to.be.true;
                 expect(graph.hasTriple(t1)).to.be.true;
                 expect(graph.hasTriple(t2)).to.be.true;
@@ -32,19 +36,12 @@ describe("TurtleReader", function () {
                 expect(prefixes.hasPrefix("ex")).to.be.true;
                 expect(prefixes.hasNamespace("http://example.org#")).to.be.true;
                 expect(prefixes.size).to.equal(1);
-                done();
-            } catch (err) {
-                done(err);
-            }
+            });
         });
 
-        it("should reject invalid input", async function (done) {
-            try {
-                await this.reader.parse(this.s1);
-                done(new Error("Accepted invalid input."));
-            } catch (err) {
-                done();
-            }
+        it("should reject invalid input", function () {
+            const p = this.reader.parse(this.s1);
+            return expect(p).to.eventually.be.rejected;
         });
     });
 
